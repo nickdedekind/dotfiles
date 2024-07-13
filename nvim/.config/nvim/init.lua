382da3ab92b89f8a42f75f65a2e18d65dc68291d
Nick Dedekind <n.dedekind@mwam.com>
2024-07-13 20:48:59 +0100

    work changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -80,6 +80,10 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("i", "jk", "<Esc>", { desc = "Move focus to the upper window" })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
     group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
---
+++ nvim/.config/nvim/lua/plugins/cmake.lua
@@ -0,0 +1,12 @@
+return {
+    "Civitasv/cmake-tools.nvim",
+    config = function()
+        require("cmake-tools").setup({
+            cmake_build_directory = function()
+                return "/build"
+            end,
+            cmake_generate_options = { "-DCMAKE_GENERATOR=Ninja" },
+            cmake_build_options = { "-j 40" },
+        })
+    end,
+}
